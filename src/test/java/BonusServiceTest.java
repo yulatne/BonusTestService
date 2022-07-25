fddaf62bab91eeae8e25a1647d35b2d7f11bc629
yulatne <tyzhina@icloud.com>
2022-07-25 19:30:56 +0300

    CommitBST

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BonusServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, false);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit2() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, false);
+
+        assertEquals(expected, actual);
+    }
+}
+
+
+
+
