98f3ec81b75fc465ee7764fc575f88362ec57d76
Yulia Tyuzhina <106819782+yulatne@users.noreply.github.com>
2022-07-30 14:32:01 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -29,7 +29,7 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
         long amount = 1000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 10;
         long actual = service.calculate(amount, false);
 
         assertEquals(expected, actual);
